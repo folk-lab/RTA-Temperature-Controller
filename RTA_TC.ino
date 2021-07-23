815988a7704c4f819dc898dc340ff735f952dbba
Anton Cecic <85712185+antoncecic@users.noreply.github.com>
2021-07-23 12:28:55 -0700

    Update RTA_TC.ino

--- RTA_TC.ino
+++ RTA_TC.ino
@@ -1,5 +1,3 @@
-/* Arduino Pro Micro Code for RTA Temperature Controller */
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -17,8 +15,10 @@ MAX6675 thermocouple(10, 16, 14);    // (SCK pin, CS pin, SO pin)
 uint8_t state = 0;
 double Setpoint, Input, Output;
 float kp = 1;
-float ki = 0.5;
-float kd = 0.5;
+float ki = 0.1;
+float kd = 0.1;
+float T;
+float ramp_time_330, ramp_time_445, PID_time_330, PID_time_445;
 
 PID myPID(&Input, &Output, &Setpoint, kp, ki, kd, DIRECT);
 
@@ -30,71 +30,91 @@ void setup() {
   Setpoint = 330;
   Input = thermocouple.readCelsius();
   myPID.SetMode(AUTOMATIC);
-  //myPID.SetSampleTime(300);
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
   delay(100);
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.display();
   delay(100);
 }
 
-
 void loop() {
-  if(state == 0){
-    ramp_fn(); 
+  if (state==0){
+    PID_fn();
   }
-  else if (state == 1){
-    PID_fn(); 
+  else if (state==1){
+    cool_fn();
   }
-  else{
-    cool_fn(); 
-  }
-}
-
-void ramp_fn(void){
-  delay(200); 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);  
-  display.println("Ramping");
-  display.print(millis()/1000);
-  display.println(" s");
-  display.print(thermocouple.readCelsius());
-  display.print((char)247);
-  display.print("C");
-  display.display();
-  
-  if(thermocouple.readCelsius() < Setpoint){
-    digitalWrite(SSR_PIN, HIGH);                // SSR On  
-  }
-  else{
-    digitalWrite(SSR_PIN, LOW);                 // SSR Off
-    state = 1;
+  else if (state==2){
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.print("DONE");
   }
 }
 
 void PID_fn(void){   
-  delay(200);    
-  Input = thermocouple.readCelsius();
+  delay(200); 
+  T = thermocouple.readCelsius(); 
+  Input = T;
   myPID.Compute();
   analogWrite(SSR_PIN, Output);
   display.clearDisplay();
-  display.setCursor(0, 0);
+  display.setCursor(0, 16);
   display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);  
-  display.println("PID");
-  display.print(millis()/1000);
-  display.println(" s");
-  display.print(thermocouple.readCelsius());
+  display.setTextColor(SSD1306_WHITE);
+  display.print(T);
   display.print((char)247);
-  display.print("C");
-  display.display();
+  display.println("C");
+
+  if (T < 320 && Setpoint==330){
+    ramp_time_330 = millis();
+    display.setTextSize(1);
+    display.println("Ramp to 330");
+    display.display();
+  }
+
+  else if (T > 320 && Setpoint==330){
+    PID_time_330 = millis() - ramp_time_330;
+    display.setTextSize(1);
+    display.println("Hold at 330 for 2 min");
+    display.print(PID_time_330/1000);
+    display.println(" s");
+    display.display();
+    
+    if (PID_time_330 > 120000){
+      Setpoint = 445;
+    }
+  }
+    
+  else if (T < 435 && Setpoint==445){
+    ramp_time_445 = millis();
+    display.setTextSize(1);
+    display.println("Ramp to 445");
+    display.display();
+  }
+
+  else if (T > 435 && Setpoint==445){
+    PID_time_445 = millis() - ramp_time_445;
+    display.setTextSize(1);
+    display.println("Hold at 445 for 2 min");
+    display.print(PID_time_445/1000);
+    display.println(" s");
+    display.display();
+    if (PID_time_445 > 120000){
+      state = 1;
+    }
+    else{
+      
+    }
+    
+  }
 }
 
 void cool_fn(void){
+  delay(200);
   digitalWrite(SSR_PIN, LOW);    //SSR Off with HIGH pulse
   display.clearDisplay();
   display.setCursor(0, 0);
@@ -107,5 +127,12 @@ void cool_fn(void){
   display.print((char)247);
   display.print("C");
   display.display();
+  if (thermocouple.readCelsius()<30){
+    state=2;
+  }
 }
+
+
+
+
     
