fc86336334a099a5d7b81b7df77b42f78f1b2942
Tim <timjchild@phas.ubc.ca>
2021-07-23 18:38:03 -0700

    Amazing - This is Ray

--- RTA_TC.ino
+++ RTA_TC.ino
@@ -1,9 +1,13 @@
 #include <SPI.h>
 #include <Wire.h>
+
+// libraries that likely need to be downloaded in library manager
+#include <PID_v1.h>
+#include <max6675.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <max6675.h>
-#include <PID_v1.h>
+
+// additional libraries included with the file
 #include <StackArray.h>
 #include "src/HeatingSchedule.h"
 
@@ -13,13 +17,7 @@
 MAX6675 thermocouple(10, 16, 14); // (SCK pin, CS pin, SO pin)
 Adafruit_SSD1306 display(128, 64, &Wire, 7);
 
-uint8_t state = 0;
-// PID related parameters
-float Setpoint, Input, Output;
-
-float ramp_time_330, ramp_time_445, PID_time_330, PID_time_445;
-
-// I ripped this from our FastDAC code!
+// Ruiheng: I ripped this from our FastDAC code!
 typedef struct PIDparam
 {
 	bool forward_dir = true;
@@ -65,7 +63,7 @@ void setup()
 
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
 	delay(100);
-	display.setTextSize(1);
+	display.setTextSize(2);
 	display.setTextColor(SSD1306_WHITE);
 	display.display();
 	delay(100);
@@ -82,9 +80,7 @@ void loop()
 	else
 	{
 		cool_fn();
-		display.clearDisplay();
-		display.setCursor(0, 0);
-		display.setTextSize(2);
+		reset_display();
 		display.print("DONE");
 		display.display();
 	}
@@ -106,18 +102,14 @@ void PID_fn(void)
 	while (setpoint - T > 10.0)
 	{
 		delay(200);
+		T = thermocouple.readCelsius();
 		g_pidparam[0].Input = T;
 		myPID.Compute();
 		analogWrite(SSR_PIN, g_pidparam[0].Output);
-		display.clearDisplay();
-		display.setCursor(0, 16);
-		display.setTextSize(2);
-		display.print(T);
-		display.print((char)247);
-		display.println("C");
-		display.setTextSize(1);
-		display.print("Ramp to ");
-		display.println(setpoint);
+		reset_display();
+
+		display.println(String(T, 2) + " C");
+		display.println("Ramp: " +  String(setpoint, 2));
 		display.display();
 	}
 
@@ -127,38 +119,30 @@ void PID_fn(void)
 	while ((millis() - start_time) / 1000.0 < ht)
 	{
 		delay(200);
+		T = thermocouple.readCelsius();
 		g_pidparam[0].Input = T;
 		myPID.Compute();
 		analogWrite(SSR_PIN, g_pidparam[0].Output);
-		display.clearDisplay();
-		display.setCursor(0, 16);
-		display.setTextSize(2);
-		display.print(T);
-		display.print((char)247);
-		display.println("C");
-		display.setTextSize(1);
-		display.print("Held for ");
-		display.print((millis() - start_time) / 1000.0);
-		display.println(" s");
+		reset_display();
+		display.println(String(T, 2) + " C");
+		display.println("Hold: " +  String((millis() - start_time)/ 1000.0, 1) + " s");
 		display.display();
 	}
 }
 
 void cool_fn(void)
 {
-	while (thermocouple.readCelsius() > 30)
+	unsigned long start_time = millis(); // in ms
+	double T = thermocouple.readCelsius();
+
+	while (T > 30)
 	{
 		delay(200);
+		T = thermocouple.readCelsius();
 		digitalWrite(SSR_PIN, LOW); //SSR Off with HIGH pulse
-		display.clearDisplay();
-		display.setCursor(0, 0);
-		display.setTextSize(2);
-		display.println("Cooling");
-		display.print(millis() / 1000);
-		display.println(" s");
-		display.print(thermocouple.readCelsius());
-		display.print((char)247);
-		display.print("C");
+		reset_display();
+		display.println(String(T, 2) + " C");
+		display.println("Cooling" + String((millis() - start_time) / 1000, 1) + " s");
 		display.display();
 	}
 }
@@ -169,4 +153,13 @@ void set_pid_tune(double kp, double ki, double kd)
 	g_pidparam[0].ki = ki;
 	g_pidparam[0].kd = kd;
 	myPID.SetTunings(g_pidparam[0].kp, g_pidparam[0].ki, g_pidparam[0].kd);
+}
+
+void reset_display(void){
+	display.clearDisplay();
+	display.setCursor(0, 16);
+}
+
+void append_to_display(String message){
+	display.println(message);
 }
