283b9295840fc15a2c500551b088ce04ad16a1cf
Ruiheng Su <53983028+sillyPhotons@users.noreply.github.com>
2021-08-10 10:40:35 -0700

    Update RTA_TC_v2.ino

--- RTA_TC_v2.ino
+++ RTA_TC_v2.ino
@@ -24,35 +24,42 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 // Ruiheng: I ripped this from our FastDAC code!
 typedef struct PIDparam
 {
-	bool forward_dir = true;
-	double Input = 0.0;
-	double Output = 0.0;
-	double Setpoint = 0.0;
-	double kp = 0.1;
-	double ki = 1.0;
-	double kd = 0.0;
+  bool forward_dir = true;
+  double Input = 0.0;
+  double Output = 0.0;
+  double Setpoint = 0.0;
+  double kp = 0.1;
+  double ki = 1.0;
+  double kd = 0.0;
 } PIDparam;
 
 PIDparam g_pidparam[1];
 
 PID myPID(&(g_pidparam[0].Input),
-		  &(g_pidparam[0].Output),
-		  &(g_pidparam[0].Setpoint),
-		  g_pidparam[0].kp,
-		  g_pidparam[0].ki,
-		  g_pidparam[0].kd,
-		  P_ON_M,
-		  DIRECT);
+          &(g_pidparam[0].Output),
+          &(g_pidparam[0].Setpoint),
+          g_pidparam[0].kp,
+          g_pidparam[0].ki,
+          g_pidparam[0].kd,
+          P_ON_M,
+          DIRECT);
 
 /*
 Modify below 
 */
-// Temperature [C], kP, kI, kD, Seconds to Hold Temperature At
-HeatingStep step0(320, 0.0, 1, 0.0, 1);
-HeatingStep step1(330, 4.00, 1.2, 0.0, 120); // Set knob to 60% full power
-HeatingStep step2(435, 0, 1.0, 0.0, 1);
-HeatingStep step3(445, 3.8, 0.9, 0.0, 120);
-HeatingStep step4(50, 0, 5.0, 0.0, 1);
+// Temperature [C], kP, kI, kD, Seconds to Hold Temperature At            // Set knob to 60% full power
+
+// Ebrahim's Sequence
+HeatingStep step0(300, 4.00, 1.2, 0.0, 3600, 6.0);
+HeatingStep step1(50, 0.0, 0.0, 0.0, 1, 0);
+
+/*
+// Ray's Sequence
+HeatingStep step0(330, 4.00, 1.2, 0.0, 120, 5.0);
+HeatingStep step1(445, 3.8, 0.9, 0.0, 120, 3.0);
+HeatingStep step2(50, 0.0, 0.0, 0.0, 1, 0);
+*/
+
 /*
 Modify above
 */
@@ -65,125 +72,167 @@ void set_pid_tune(double, double, double);
 
 void setup()
 {
-	/*
-	Modify below 
-	*/
-	heating_schedule.push(step4);
-	heating_schedule.push(step3);
-	heating_schedule.push(step2);
-	heating_schedule.push(step1);
+  Serial.begin(9600);
+  /*
+  Modify below 
+  */
+  //heating_schedule.push(step2);
+  heating_schedule.push(step1);
   heating_schedule.push(step0);
-	/*
-	Modify above
-	*/
+  /*
+  Modify above
+  */
 
-	delay(2000); // do not remove this delay!
-	pinMode(SSR_PIN, OUTPUT);
-	pinMode(START_PIN, INPUT);
-	digitalWrite(SSR_PIN, LOW);
+  delay(2000); // do not remove this delay!
+  pinMode(SSR_PIN, OUTPUT);
+  pinMode(START_PIN, INPUT);
+  digitalWrite(SSR_PIN, LOW);
 
-	g_pidparam[0].Input = probe.readTempC();
- 	myPID.SetSampleTime(50);
-	myPID.SetMode(AUTOMATIC);
-	myPID.SetOutputLimits(0, 255); // although the function defaults to 0 to 255, we call this anyway to be safe
+  g_pidparam[0].Input = probe.readTempC();
+  myPID.SetSampleTime(50);
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(0, 255); // although the function defaults to 0 to 255, we call this anyway to be safe
 
-	display.begin(SSD1306_SWITCHCAPVCC, 0);
-	display.setTextColor(SSD1306_WHITE);
-	display.setTextSize(2);
-	reset_display();
-	delay(100);
-	display.drawBitmap(0, 0, myBitmap, 125, 65, WHITE);
-	delay(100);
-	display.display();
-	delay(2000);
-	reset_display();
-	delay(100);
+  display.begin(SSD1306_SWITCHCAPVCC, 0);
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
+  reset_display();
+  delay(100);
+  display.drawBitmap(0, 0, myBitmap, 125, 65, WHITE);
+  delay(100);
+  display.display();
+  delay(2000);
+  reset_display();
+  delay(100);
 }
 
 void loop()
 {
-	if (START == HIGH){
-		if (!heating_schedule.isEmpty())
-		{
-			PID_fn();
-			// after ramping, holding
-			heating_schedule.pop(); // pop the last entry from the stack
-		}
-		else
-		{
-			reset_display();
-			display.println("COMPLETE");
-			display.display();
-		}		
-	}
-	else{
-		delay(100);
-		START = digitalRead(START_PIN);
-		reset_display();
-		display.println("READY");
-		display.println(String(probe.readTempC(), 2) + String(char(247)) + "C");
-		display.display();
-	}
+  if (START == HIGH)
+  {
+    if (!heating_schedule.isEmpty())
+    {
+      PID_fn();
+      // after ramping, holding
+      heating_schedule.pop(); // pop the last entry from the stack
+    }
+    else{
+      delay(1000);
+      analogWrite(SSR_PIN, 0);
+      delay(1000);
+      START = LOW;
+    }
+  }
+  else
+  {
+    delay(110);
+    START = digitalRead(START_PIN);
+    reset_display();
+    display.println("READY");
+
+    double T = probe.readTempC();
+    Serial.println(T);
+    display.println(String(T, 2) + String(char(247)) + "C");
+    display.display();
+  }
 }
 
 void PID_fn(void)
 {
-	double setpoint = heating_schedule.peek().setpoint;
-	double kp = heating_schedule.peek().proportional;
-	double ki = heating_schedule.peek().integral;
-	double kd = heating_schedule.peek().derivative;
-	double ht = heating_schedule.peek().hold_time;
-	double T = probe.readTempC();
+  double setpoint = heating_schedule.peek().setpoint;
+  double kp = heating_schedule.peek().proportional;
+  double ki = heating_schedule.peek().integral;
+  double kd = heating_schedule.peek().derivative;
+  double ht = heating_schedule.peek().hold_time;
+  // temperature difference in ramp 
+  double delta_t = heating_schedule.peek().delta_t;
+  
+  // read the temperature
+  double T = probe.readTempC();
+  
+  // set the PID parameter
+  set_pid_tune(kp, ki, kd);
+  g_pidparam[0].Setpoint = setpoint;
 
-	set_pid_tune(kp, ki, kd);
-	g_pidparam[0].Setpoint = setpoint;
+  // if the setpoint is delta_t higher, then we close the relay fully 
+  while ((setpoint - T) >= delta_t)
+  {
+    delay(110);
+    T = probe.readTempC();
+    Serial.println(T);
+    analogWrite(SSR_PIN, 255);
+    reset_display();
+    display.println(String(T, 2) + String(char(247)) + "C");
+    display.println("Ramp to:");
+    display.println(String(setpoint, 0) + String(char(247)) + "C");
+    display.display();
+  }
+  
+  // if the overshoot is more than delta_t then we leave the relay open 
+  while ((T - setpoint) >= delta_t)
+  {
+    delay(110);
+    T = probe.readTempC();
+    Serial.println(T);
+    analogWrite(SSR_PIN, 0);
+    reset_display();
+    display.println(String(T, 2) + String(char(247)) + "C");
+    display.println("Cool to:");
+    display.println(String(setpoint, 0) + String(char(247)) + "C");
+    display.display();
+  }
+  
+  // the temperature is within delta_t of the setpoint, but more than 1 C away from it 
+  while (abs(setpoint - T) > 1.0)
+  {
+    delay(110);
+    T = probe.readTempC();
+    Serial.println(T);
+    g_pidparam[0].Input = T;
+    myPID.Compute();
+    analogWrite(SSR_PIN, g_pidparam[0].Output);
+    reset_display();
+    display.println(String(T, 2) + String(char(247)) + "C");
+    display.println("Ramp to:");
+    display.println(String(setpoint, 0) + String(char(247)) + "C");
+    display.display();
+  }
 
-	// ramping sequence
-	while (abs(setpoint - T) > 1.0 && abs(setpoint - T) < 10.0)
-	{
-		delay(100);
-		T = probe.readTempC();
-		g_pidparam[0].Input = T;
-		myPID.Compute();
-		analogWrite(SSR_PIN, g_pidparam[0].Output);
-		reset_display();
-		display.println(String(T, 2)+ String(char(247)) + "C");
-		display.println("Ramp " + String(setpoint, 0) + String(char(247)) + "C");
-		display.display();
-	}
+  unsigned long start_time = millis(); // in ms
 
-	unsigned long start_time = millis(); // in ms
-
-	// holding
-	while ((millis() - start_time) / 1000.0 < ht)
-	{
-		delay(100);
-		T = probe.readTempC();
-		g_pidparam[0].Input = T;
-		myPID.Compute();
-		analogWrite(SSR_PIN, g_pidparam[0].Output);
-		reset_display();
-		display.println(String(T, 2) + String(char(247)) + "C");
-		display.println("Holding " + String((millis() - start_time) / 1000, 1) + "s");
-		display.display();
-	}
+  // holding
+  while ((millis() - start_time) / 1000.0 < ht)
+  {
+    delay(110);
+    T = probe.readTempC();
+    Serial.println(T);
+    g_pidparam[0].Input = T;
+    myPID.Compute();
+    analogWrite(SSR_PIN, g_pidparam[0].Output);
+    reset_display();
+    display.println(String(T, 2) + String(char(247)) + "C");
+    display.println("Holding");
+    display.print((millis() - start_time) / 1000);
+    display.println("s");
+    display.display();
+  }
 }
 
 void set_pid_tune(double kp, double ki, double kd)
 {
-	g_pidparam[0].kp = kp;
-	g_pidparam[0].ki = ki;
-	g_pidparam[0].kd = kd;
-	myPID.SetTunings(g_pidparam[0].kp, g_pidparam[0].ki, g_pidparam[0].kd);
+  g_pidparam[0].kp = kp;
+  g_pidparam[0].ki = ki;
+  g_pidparam[0].kd = kd;
+  myPID.SetTunings(g_pidparam[0].kp, g_pidparam[0].ki, g_pidparam[0].kd);
 }
 
 void reset_display(void)
 {
-	display.clearDisplay();
-	display.setCursor(0, 0);
+  display.clearDisplay();
+  display.setCursor(0, 0);
 }
 
 void append_to_display(String message)
 {
-	display.println(message);
+  display.println(message);
 }
